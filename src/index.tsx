caf596a523435f6bc502ba9c0f6a865943cd7ae2
3293232930@qq.com <xu984632>
2021-04-10 20:05:54 +0800

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React                            from 'react'
+import ReactDOM                         from 'react-dom'
+import SignIn                           from './views/system/Login'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Error404                         from './views/error-page/404'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <React.StrictMode>
+      <BrowserRouter>
+        <Switch>
+          <Route path='/sign-in' component={SignIn} />
+          <Route component={Error404}/>
+        </Switch>
+      </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
+)
